9aed15ad637d44ec83e5e9255ac92d5ce44b6746
yassineboy <boyy62811@gmail.com>
2025-06-02 20:12:56 +0200

    Delete PS3HEN directory

--- PS3HEN/c-cex.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal enabledelayedexpansion
-
-FOR /L %%A IN (80,1,92) DO (
-xcopy /Y PS3HEN.BIN_CEX_4%%A C:\PS3HEN-en\Make_PKG\4.%%A\dev_rewrite\hen\PS3HEN.BIN
-xcopy /Y PS3HEN.BIN_CEX_4%%A C:\PS3HEN-en\Make_PKG\4.%%A\dev_hdd0\hen\m\r\HEN.BIN
-xcopy /Y PS3HEN.BIN_CEX_4%%A C:\Github\nikolaevich23.github.io\alt\4.%%A\PS3HEN.p3t
-)
--- PS3HEN/payload/lite.h
+++
@@ -1,7 +0,0 @@
-#ifndef __LITE_H__
-#define __LITE_H__
-
-static int mappath_disabled=0;
-
-#endif /* __LITE_H__ */
-
